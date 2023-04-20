bfec511609e4cb8a23512a31bafad8bc792c53a0
gmarvis <sgmarvis@gmail.com>
2023-04-20 17:31:24 +0100

    complete css design

--- main.js
+++ main.js
@@ -10,11 +10,50 @@ form.addEventListener('submit', function(event){
     event.preventDefault();
 // const addlist = JSON.parse(todoInput.value)
 
+// stop empty todo
+
+if (!todoInput.value){
+    return alert('Please enter a task')
+}
+
+// Create a new li
+
     let newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value
     todoContainer.appendChild(newTodo)
     todoInput.value = ""
+
+    const actionBtns = document.createElement('span');
+    newTodo.appendChild(actionBtns)
+
+    // Create to complete
+    const completeBtn = document.createElement('button')
+    completeBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    actionBtns.appendChild(completeBtn)
+
+    // create the delete btn 
+    const deletebtn = document.createElement('button')
+    deletebtn.innerHTML = '<i class="fa-solid fa-trash"></i></i>'
+    actionBtns.appendChild(deletebtn)
+
+    // completed todo funtion
+    completeBtn.addEventListener('click' , function(){
+        todoContainer[0]=newTodo.style.textDecoration="line-through"
+    })
+
+        // delete todo funtion
+        deletebtn.addEventListener('click', function(){
+            newTodo.remove()
+        })
+
+
+
 })
+
+
+
+
+
 
 
 
